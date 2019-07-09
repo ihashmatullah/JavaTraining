7a20dade9ed13877af44dc171d50ab9aa9bea6bf
Hashmatullah Noorzai <hashmatullah.noorzai@dev-intern2.vgnet.volgrp.com>
2019-07-09 15:43:22 -0500

    Completed Password check assignment

--- src/labs/PasswordCriteria.java
+++ src/labs/PasswordCriteria.java
@@ -5,19 +5,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PasswordCriteria {
 
 	public static void main(String[] args) {
 		
-		//	1. Read a file that contains multiple sets of passwords (use http://www.theonegenerator.com/ to generate a set of test data)
+		//	1. Read a file that contains multiple sets of passwords 
+		//	(use http://www.theonegenerator.com/ to generate a set of test data)
 			
 			// Define the path of file
 		String filename = "/Users/hashmatullah.noorzai/eclipse-workspace/JavaTraining/Files/Passwords.txt";
 			// Create the file in Java
 		File file = new File(filename);
 		String[] passwords = new String[13];
-		String password = null;	
+		String password = null;
 		try {
 			// Create a file reader that reads from the file
 			BufferedReader br = new BufferedReader(new FileReader(file));
@@ -33,31 +36,72 @@ public class PasswordCriteria {
 			System.out.println("Error: Could not read file");
 		}
 		
-		// Print stored passwords in passwords array to the user
+			// Print stored passwords in passwords_array to the user
 		for(int i=0; i<passwords.length; i++) {
 			password = passwords[i];
+		//	2. Verify that the password contains all of the following criteria:
 			try {
-				if (password) {
-					
+				//		1. A number
+				Pattern numP = Pattern.compile(".*\\d.*");
+				Matcher numM = numP.matcher(password);
+				if (!numM.find()) {
+					throw new NumberException(password);
 				}
-			} catch () {
-				
+				//		2. A letter
+				Pattern letterP = Pattern.compile(".*[a-zA-Z]");
+				Matcher letterM = letterP.matcher(password);
+				if (!letterM.find()) {
+					throw new LetterException(password);
+				}
+				//		3. Special character (! @ #)
+				Pattern spcCharP = Pattern.compile(".*[!@#]");
+				Matcher spcCharM = spcCharP.matcher(password);
+				if (!spcCharM.find()) {
+					throw new SpecialCharException(password);
+				}
+				System.out.println(password);
+		//	4. If the password does not meet the criteria, create and throw the appropriate exception
+			} catch (NumberException e) {
+				System.out.println("ERROR: Password requires a number");
+				System.out.println(e.toString());
+			} catch (LetterException e) {
+				System.out.println("ERROR: Password requires a letter");
+				System.out.println(e.toString());
+			} catch (SpecialCharException e) {
+				System.out.println("ERROR: Password requires a special character, choose from: !@#");
+				System.out.println(e.toString());
 			}
 		}
-			System.out.println(password);
-		}
-		
-		
-		
-		
-		//	2. Verify that the password contains all of the following criteria:
-		//		1. A number
-		//		2. A letter
-		//		3. Special character (! @ #)
-
-		//	3. Create three User Defined Exceptions for the corresponding criteria.
-
-		//	4. If the password does not meet the criteria, create and throw the appropriate exception
 	}
+}
 
+//	3. Create three User Defined Exceptions for the corresponding criteria.
+class NumberException extends Exception {
+	String numInPassword;
+	NumberException(String numInPassword) {
+		this.numInPassword = numInPassword;
+	}
+	public String toString() {
+		return ("NumberException: " + numInPassword);
+	}
+}
+
+class LetterException extends Exception {
+	String letterInPassword;
+	LetterException(String letterInPassword) {
+		this.letterInPassword = letterInPassword;
+	}
+	public String toString() {
+		return ("LetterException: " + letterInPassword);
+	}
+}
+
+class SpecialCharException extends Exception {
+	String specialCharInPassword;
+	SpecialCharException(String specialCharInPassword) {
+		this.specialCharInPassword = specialCharInPassword;
+	}
+	public String toString() {
+		return ("SpecialCharException: " + specialCharInPassword);
+	}
 }
